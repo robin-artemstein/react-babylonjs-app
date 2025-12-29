9db4f83cd9cd789d6b9938eebab754b9efbc775f
robin-artemstein <51808727+RobinArtemstein@users.noreply.github.com>
2025-12-29 19:53:33 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <div className="flex flex-col justify-center">
+      <div className="flex flex-col justify-center gap-4">
         <div className="font-bold text-3xl text-lime-300">Babylon.js + React + Vite + React testing application...</div>
         <div><BabylonScene /></div>
       </div>

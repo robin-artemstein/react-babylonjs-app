ee1cdbb24b471a23b6f97e0bec69034a4d135fdc
robin-artemstein <51808727+RobinArtemstein@users.noreply.github.com>
2026-02-09 20:48:10 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import BabylonScene from "./BabylonScene";
 
 function App() {
-
+  const modelLink: string = "https://raw.githubusercontent.com/robin-artemstein/testing-static-pages/main/";
+  const modelName: string = "Katana3.glb"
   return (
     <>
       <div className="flex flex-col justify-center gap-4">
         <div className="font-bold text-3xl text-lime-300">Babylon.js + React + Vite + React testing application...</div>
-        <div><BabylonScene modelSourceLink={"https://raw.githubusercontent.com/robin-artemstein/testing-static-pages/main/"} modelSourceName={"Katana2.glb"} /></div>
+        <div><BabylonScene modelSourceLink={modelLink} modelSourceName={modelName} /></div>
       </div>
     </>
   )
